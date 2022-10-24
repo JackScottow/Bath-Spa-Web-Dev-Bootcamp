48c8cd6c97fdd34d8161c874e8ee6df72c100841
JackScottow <101928816+JackScottow@users.noreply.github.com>
2022-10-24 11:05:16 +0100

    Lightbulb

--- Session-10/Lightbulb/script.js
+++ Session-10/Lightbulb/script.js
@@ -1,6 +1,14 @@
 function turnOn() {
   document.getElementById("bulb").src = "img/on.gif";
+  document.body.style.backgroundColor = "white";
+  window.alert("Phew! That was scary!");
 }
+
 function turnOff() {
-  document.getElementById("bulb").src = "img/off.gif";
+  if (confirm("Are you sure? It's scary in the dark!") == true) {
+    document.getElementById("bulb").src = "img/off.gif";
+    document.body.style.backgroundColor = "black";
+  } else {
+    window.alert("Good idea!");
+  }
 }
