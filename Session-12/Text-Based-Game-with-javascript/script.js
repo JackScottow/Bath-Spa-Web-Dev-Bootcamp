7d73b56993f2b76b862bc6bc84c1281e9dc8209e
JackScottow <101928816+JackScottow@users.noreply.github.com>
2022-10-30 11:22:02 +0000

    Completed game

--- Session-12/Text-Based-Game-with-javascript/script.js
+++ Session-12/Text-Based-Game-with-javascript/script.js
@@ -1,46 +1,163 @@
 var start = document.getElementById("start");
-var content = document.getElementById("container");
 var image = document.getElementById("img");
 var title = document.getElementById("title");
-var desc = document.getElementById("desc");
-var enterName = document.getElementById("entername");
-var option1, option2;
-var button1 = document.getElementById("a");
-var button2 = document.getElementById("b");
+var text = document.getElementById("text");
+var enterName = document.getElementById("enterName");
+var buttonBox = document.getElementById("buttonBox");
 var charName;
-const buttonOptions = [
-  ["Make Fire", "Take Weapon And Walk"],
-  ["C", "D"],
-];
+var scenario = {
+  one: {
+    title: "The beginning",
+    image: "img/start.jpg",
+    text: "Welcome, charName! You wake up delirious in the middle of a forest camp, next to you are some items to make a fire and a weapon",
+    buttons: [
+      ["Make Fire", "advanceTo(scenario.two)"],
+      ["Take Weapon And Walk", "advanceTo(scenario.three)"],
+    ],
+  },
+  two: {
+    title: "Fire",
+    image: "img/fire.jpg",
+    text: "You have made a fire but have no food to put on it, you should go and find some",
+    buttons: [["Take Weapon And Walk", "advanceTo(scenario.three)"]],
+  },
+  three: {
+    title: "Oh no! A boar!",
+    image: "img/boar.jpg",
+    text: "You have encountered a wild boar, what do you want to do?",
+    buttons: [
+      ["Kill it", "advanceTo(scenario.four)"],
+      ["Run away", "advanceTo(scenario.five)"],
+    ],
+  },
+  four: {
+    title: "Oh no! A dead boar!",
+    image: "img/dead-boar.jpg",
+    text: "Well done charName! You have slain the boar! Do you want to return to camp to cook it or carry on?",
+    buttons: [
+      ["Return to camp", "advanceTo(scenario.six)"],
+      ["Carry on", "advanceTo(scenario.eleven)"], //
+    ],
+  },
+  five: {
+    title: "You failed!",
+    image: "img/run.jpg",
+    text: "You run as fast as you can never to be seen again, better luck next time, charName!",
+    buttons: [["Try again", "window.location.reload();"]],
+  },
+  six: {
+    title: "The feast",
+    image: "img/cook.jpg",
+    text: "You are back at camp and have somehow rustled up a delicious hog roast, wow charName! What a  great chef you are!",
+    buttons: [
+      ["Go to sleep", "advanceTo(scenario.seven)"],
+      ["Head out again", "advanceTo(scenario.twelve)"], //
+    ],
+  },
+  seven: {
+    title: "z Z z Z z Z z",
+    image: "img/sleep.jpg",
+    text: "You sure were tired, charName!",
+    buttons: [
+      ["Sleep some more", "advanceTo(scenario.seven)"],
+      ["Hear a noise and wake up", "advanceTo(scenario.eight)"],
+    ],
+  },
+  eight: {
+    title: "Hello? Who's there!?",
+    image: "img/wake-up.jpg",
+    text: "Uh oh, charName! You have some intruders, they don't look very friendly!",
+    buttons: [
+      ["Stand and fight", "advanceTo(scenario.nine)"],
+      ["Run away", "advanceTo(scenario.five)"],
+    ],
+  },
+  nine: {
+    title: "You defeated the enemies!?",
+    image: "img/defeated.jpg",
+    text: "Well done charName! They put up a hell of a fight!",
+    buttons: [["Victory!", "advanceTo(scenario.ten)"]],
+  },
+  ten: {
+    title: "Congratulations!",
+    image: "img/win.jpg",
+    text: "Well done charName! You have beaten the game!",
+    buttons: [["Start again", "window.location.reload();"]],
+  },
+  eleven: {
+    title: "A lonely walk...",
+    image: "img/carry-on.jpg",
+    text: "You walk through the wildnerness for what seems like forever, don't forget charName! You still have the boar meat with you.",
+    buttons: [
+      ["Pick some berries", "advanceTo(scenario.fourteen)"],
+      ["Go back to eat", "advanceTo(scenario.six)"],
+    ],
+  },
+  twelve: {
+    title: "A lonely walk...",
+    image: "img/carry-on.jpg",
+    text: "You walk through the wildnerness for what seems like forever, maybe you should return to camp to sleep?",
+    buttons: [
+      ["Pick some berries", "advanceTo(scenario.thirteen)"],
+      ["Go back to sleep", "advanceTo(scenario.seven)"],
+    ],
+  },
+  thirteen: {
+    title: "Who doesn't love berries?",
+    image: "img/berries.jpg",
+    text: "You have filled your pockets with berries but now it's definetly time to return to camp for some sleep!",
+    buttons: [["Go back to sleep", "advanceTo(scenario.seven)"]],
+  },
+  fourteen: {
+    title: "Who doesn't love berries?",
+    image: "img/berries.jpg",
+    text: "You have filled your pockets with berries, they would be lovely with the boar meat!",
+    buttons: [["Go back to camp", "advanceTo(scenario.fifteen)"]],
+  },
+  fifteen: {
+    title: "The feast",
+    image: "img/cook.jpg",
+    text: "You are back at camp and have somehow rustled up a delicious hog roast with berries, wow charName! What a  great chef you are! You are thoroughly exhausted now though!",
+    buttons: [["Go to sleep", "advanceTo(scenario.seven)"]],
+  },
+};
 
 start.addEventListener("click", () => {
   charName = document.getElementById("name").value;
   if (charName == "") {
-    window.alert("You need a name!");
+    window.alert("Your character needs a name!");
   } else {
-    image.setAttribute("src", "img/start.jpg");
-    desc.innerText =
-      "Welcome, " + charName + "! You wake up delirious in the middle of a forest camp, next to you are some items to make a fire and a weapon";
-    option1 = buttonOptions[0][0];
-    option2 = buttonOptions[0][1];
+    advanceTo(scenario.one);
     enterName.remove();
-    start.remove();
-    button1.classList.remove("hide");
-    button2.classList.remove("hide");
-    button1.innerText = option1;
-    button2.innerText = option2;
   }
 });
 
-button1.addEventListener("click", () => {
-  title.innerText = "Fire";
-  desc.innerText = "You have made a fire but have no food to put on it";
-  image.setAttribute("src", "img/fire.jpg");
-  button1.classList.add("hide");
-});
+var advanceTo = function (s) {
+  changeTitle(s.title);
+  changeImage(s.image);
+  changeText(s.text);
+  changeButtons(s.buttons);
+};
 
-button2.addEventListener("click", () => {
-  title.innerText = "Boar";
-  desc.innerText = "You encounter a wild boar";
-  image.setAttribute("src", "img/boar.jpg");
-});
+var changeTitle = function (header) {
+  title.innerHTML = "&nbsp" + header + "&nbsp";
+};
+
+var changeText = function (words) {
+  text.innerHTML = words.replace("charName", "<span class='charName'>" + charName + "</span>");
+};
+
+var changeImage = function (img) {
+  image.setAttribute("src", img);
+};
+
+var changeButtons = function (buttonList) {
+  buttonBox.innerHTML = "";
+  if (buttonList.length == 1) {
+    buttonBox.innerHTML += "<button onClick=" + buttonList[0][1] + ">" + buttonList[0][0] + "</button>";
+  } else {
+    for (var i = 0; i < buttonList.length; i++) {
+      buttonBox.innerHTML += "<button onClick=" + buttonList[i][1] + ">" + buttonList[i][0] + "</button>";
+    }
+  }
+};
