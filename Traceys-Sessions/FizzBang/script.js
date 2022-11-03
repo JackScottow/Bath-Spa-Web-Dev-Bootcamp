d602ae2bb439570a9fe1c1cec4c107708ab3f53f
JackScottow <101928816+JackScottow@users.noreply.github.com>
2022-11-03 19:34:23 +0000

    Colors

--- Traceys-Sessions/FizzBang/script.js
+++ Traceys-Sessions/FizzBang/script.js
@@ -3,11 +3,11 @@ var container = document.getElementById("container");
 function fizzBang() {
   for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 != 0) {
-      container.innerHTML += "<div>" + i + "<br> Fizz </div>";
+      container.innerHTML += "<div class='fizz'>" + i + "<br> Fizz </div>";
     } else if (i % 5 === 0 && i % 3 != 0) {
-      container.innerHTML += "<div>" + i + "<br> Bang </div>";
+      container.innerHTML += "<div class='bang'>" + i + "<br> Bang </div>";
     } else if (i % 5 === 0 && i % 5 === 0) {
-      container.innerHTML += "<div>" + i + "<br>  FizzBang </div>";
+      container.innerHTML += "<div class='fizzbang'>" + i + "<br>  FizzBang </div>";
     } else {
       container.innerHTML += "<div>" + i + "</div>";
     }
