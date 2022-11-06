920aea1c7cb668bc67944ae21d2427c624ab0880
JackScottow <101928816+JackScottow@users.noreply.github.com>
2022-11-06 16:14:47 +0000

    Added keypress functionality

--- Session-15/Javascript Calculator/v2/script.js
+++ Session-15/Javascript Calculator/v2/script.js
@@ -5,52 +5,15 @@ var calculate = document.getElementById("calculate");
 var deleteX = document.getElementById("delete");
 var clear = document.getElementById("clear");
 var op = ["+", "-", "×", "÷"];
+var opKeys = ["+", "-", "*", "/"];
+var keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
 
-for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener("click", () => {
-    if (numbers[i].innerText === "C") {
-      display.setAttribute("style", "text-align:center; color:red; padding:0");
-      display.innerText = "* * * * * *";
-      setTimeout(function () {
-        display.setAttribute("style", "text-align:left");
-        display.innerText = "";
-      }, 300);
-    } else {
-      display.innerText += numbers[i].innerText;
-    }
-  });
-}
+deleteFn = () => {
+  var currentVal = display.innerText;
+  display.innerText = currentVal.slice(0, -1);
+};
 
-for (let i = 0; i < operators.length; i++) {
-  operators[i].addEventListener("click", () => {
-    var currentVal = display.innerText;
-    var lastVal = currentVal[currentVal.length - 1];
-    console.log(currentVal);
-    if (operators[i].innerText === "+") {
-      if (op.includes(lastVal)) {
-        display.innerText = currentVal.slice(0, -1);
-      }
-      display.innerText += "+";
-    } else if (operators[i].innerText === "-") {
-      if (op.includes(lastVal)) {
-        display.innerText = currentVal.slice(0, -1);
-      }
-      display.innerText += "-";
-    } else if (operators[i].innerText === "×") {
-      if (op.includes(lastVal)) {
-        display.innerText = currentVal.slice(0, -1);
-      }
-      display.innerText += "×";
-    } else if (operators[i].innerText === "÷") {
-      if (op.includes(lastVal)) {
-        display.innerText = currentVal.slice(0, -1);
-      }
-      display.innerText += "÷";
-    }
-  });
-}
-
-calculate.addEventListener("click", () => {
+calculateFn = function () {
   var inputString = display.innerText;
   var numberArr = inputString.split(/\+|\-|\×|\÷/g);
   var operatorArr = inputString.replace(/[0-9]|\./g, "").split("");
@@ -84,7 +47,66 @@ calculate.addEventListener("click", () => {
   }
 
   display.innerText = numberArr[0];
+};
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", () => {
+    if (numbers[i].innerText === "C") {
+      display.setAttribute("style", "text-align:center; color:red; padding:0");
+      display.innerText = "* * * * * *";
+      setTimeout(function () {
+        display.setAttribute("style", "text-align:left");
+        display.innerText = "";
+      }, 300);
+    } else {
+      display.innerText += numbers[i].innerText;
+    }
+  });
+}
+
+window.addEventListener("keydown", (e) => {
+  console.log(e.key);
+  if (keys.includes(e.key)) {
+    display.innerText += e.key;
+  } else if (opKeys.includes(e.key)) {
+    display.innerText += op[opKeys.indexOf(e.key)];
+  } else if (e.key == "Backspace") {
+    deleteFn();
+  } else if (e.key == "Enter") {
+    calculateFn();
+  }
 });
+
+for (let i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", () => {
+    var currentVal = display.innerText;
+    var lastVal = currentVal[currentVal.length - 1];
+    console.log(currentVal);
+    if (operators[i].innerText === "+") {
+      if (op.includes(lastVal)) {
+        display.innerText = currentVal.slice(0, -1);
+      }
+      display.innerText += "+";
+    } else if (operators[i].innerText === "-") {
+      if (op.includes(lastVal)) {
+        display.innerText = currentVal.slice(0, -1);
+      }
+      display.innerText += "-";
+    } else if (operators[i].innerText === "×") {
+      if (op.includes(lastVal)) {
+        display.innerText = currentVal.slice(0, -1);
+      }
+      display.innerText += "×";
+    } else if (operators[i].innerText === "÷") {
+      if (op.includes(lastVal)) {
+        display.innerText = currentVal.slice(0, -1);
+      }
+      display.innerText += "÷";
+    }
+  });
+}
+
+calculate.addEventListener("click", calculateFn);
 
 // calculate.addEventListener("click", () => {
 //   var currentVal = display.innerText;
@@ -97,11 +119,7 @@ calculate.addEventListener("click", () => {
 //   display.innerText = Function("return " + displayReplaced)();
 // });
 
-deleteX.addEventListener("click", () => {
-  var currentVal = display.innerText;
-  // var lastVal = currentVal[currentVal.length - 1];
-  display.innerText = currentVal.slice(0, -1);
-});
+deleteX.addEventListener("click", deleteFn);
 
 // divide
 // multiply
