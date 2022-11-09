84bbf7df9d0a66ee752a701e2c1f7d15a0f36fac
JackScottow <101928816+JackScottow@users.noreply.github.com>
2022-11-09 19:04:40 +0000

    Functional

--- Session-17/Tic-Tac-Toe/script.js
+++ Session-17/Tic-Tac-Toe/script.js
@@ -1,29 +1,66 @@
 var squares = document.querySelectorAll("#board div");
 var player1 = true;
-var arr = [
-  ["", "", ""],
-  ["", "", ""],
-  ["", "", ""],
+var text = document.getElementById("text");
+var arr = [".", ".", ".", ".", ".", ".", ".", ".", "."];
+var noWin = true;
+
+const winArr = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
 squares.forEach((square) => {
   square.addEventListener("click", () => {
-    if (player1) {
-      if (arr[square.dataset.x][square.dataset.y] == "") {
-        arr[square.dataset.x][square.dataset.y] = "x";
-        square.setAttribute("class", "cross");
-        player1 = false;
+    const tileNum = square.dataset.x;
+    if (noWin) {
+      if (player1) {
+        if (arr[tileNum] == ".") {
+          arr[tileNum] = "X";
+          square.classList.add("cross");
+          player1 = false;
+          text.innerHTML = "O Turn";
+          checkWin();
+        } else {
+          window.alert("No!");
+        }
       } else {
-        window.alert("No!");
-      }
-    } else {
-      if (arr[square.dataset.x][square.dataset.y] == "") {
-        arr[square.dataset.x][square.dataset.y] = "o";
-        square.setAttribute("class", "nought");
-        player1 = true;
-      } else {
-        window.alert("No!");
+        if (arr[tileNum] == ".") {
+          arr[tileNum] = "O";
+          square.classList.add("nought");
+          player1 = true;
+          text.innerHTML = "X Turn";
+          checkWin();
+        } else {
+          window.alert("No!");
+        }
       }
     }
   });
 });
+
+const checkWin = () => {
+  for (let i = 0; i < winArr.length; i++) {
+    if (arr[winArr[i][0]] != "." && arr[winArr[i][0]] === arr[winArr[i][1]] && arr[winArr[i][1]] === arr[winArr[i][2]]) {
+      text.innerText = `${arr[winArr[i][0]]} Wins!`;
+      noWin = false;
+    } else if (arr.indexOf(".") == -1) {
+      text.innerText = "Draw!";
+    }
+  }
+};
+
+const resetFn = () => {
+  squares.forEach((square) => {
+    square.classList.remove("cross");
+    square.classList.remove("nought");
+  });
+  text.innerHTML = "X Turn";
+  noWin = true;
+  arr = [".", ".", ".", ".", ".", ".", ".", ".", "."];
+};
