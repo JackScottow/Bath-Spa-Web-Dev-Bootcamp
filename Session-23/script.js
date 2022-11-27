b9d0c46c292aa30a0b335f0eaa98a1d769e41c1d
JackScottow <101928816+JackScottow@users.noreply.github.com>
2022-11-27 12:31:48 +0000

    Filtered List

--- Session-23/script.js
+++ Session-23/script.js
@@ -1,5 +1,6 @@
-const content = document.querySelector("#content");
-
+const table = document.querySelector("#table-body");
+const filter = document.getElementById("filter");
+const submitButton = document.querySelector("#submit");
 const cars = [
   { make: "BMW", model: "330D", colour: "Silver", year: 2015 },
   { make: "Audi", model: "A5", colour: "Grey", year: 2017 },
@@ -7,36 +8,45 @@ const cars = [
   { make: "Vauxhall", model: "Corsa", colour: "Black", year: 2008 },
 ];
 
-const updateCars = () => {
-  content.innerHTML = "";
+const updateCars = (cars) => {
+  table.innerHTML = ""; // Reset table
   cars.forEach((car) => {
-    content.innerHTML += `
-          <p>
-          Make: ${car.make} 
-          Model: ${car.model}
-          Colour: ${car.colour}
-          Year: ${car.year}
-          </p>`;
+    table.innerHTML += `<tr>
+    <td>${car.make}</td>
+    <td>${car.model}</td>
+    <td>${car.colour}</td>
+    <td>${car.year}</td>
+    </tr>`;
   });
 };
 
-function Car(carMake, carModel, carColour, carYear) {
-  this.make = carMake;
-  this.model = carModel;
-  this.colour = carColour;
-  this.year = carYear;
+function Car(make, model, colour, year) {
+  this.make = make;
+  this.model = model;
+  this.colour = colour;
+  this.year = year;
 }
 
-const submitButton = document.querySelector("#submit");
-
 submitButton.addEventListener("click", () => {
-  let carMake = document.querySelector("#make").value;
-  let carModel = document.querySelector("#model").value;
-  let carColour = document.querySelector("#colour").value;
-  let carYear = document.querySelector("#year").value;
-  let newCar = new Car(carMake, carModel, carColour, carYear);
+  let make = document.querySelector("#make").value;
+  let model = document.querySelector("#model").value;
+  let colour = document.querySelector("#colour").value;
+  let year = document.querySelector("#year").value;
+  let newCar = new Car(make, model, colour, year);
   cars.push(JSON.parse(JSON.stringify(newCar)));
-  updateCars();
+  updateCars(cars);
 });
 
-updateCars();
+filter.addEventListener("keyup", (keyword) => {
+  keyword = filter.value;
+  let filteredCars = cars.filter(
+    (car) =>
+      car.make.toUpperCase().includes(keyword.toUpperCase()) ||
+      car.model.toUpperCase().includes(keyword.toUpperCase()) ||
+      car.colour.toUpperCase().includes(keyword.toUpperCase()) ||
+      car.year.toString().includes(keyword)
+  );
+  updateCars(filteredCars);
+});
+
+updateCars(cars);
