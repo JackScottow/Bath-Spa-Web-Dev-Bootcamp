53cfd8bad11164d6ecf0371fe9dbc19d8e816f92
JackScottow <101928816+JackScottow@users.noreply.github.com>
2022-12-01 15:05:42 +0000

    Dashboard mood

--- Session-28/routing/src/Pages/Dashboard.js
+++ Session-28/routing/src/Pages/Dashboard.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 function Dashboard() {
+  let [mood, setMood] = useState("How are you feeling?");
+  let [moodPic, setMoodPic] = useState("images/happy.svg");
+
+  const changeMood = (e) => {
+    setMood((mood = "You are " + e.target.innerHTML));
+    setMoodPic((moodPic = "images/" + e.target.dataset.mood + ".svg"));
+  };
+
   return (
-    <div className="Dashboard">
+    <div className="dashboard">
       <h1 className="header">Dashboard</h1>
+      <p className="mood">{mood}</p>
+      <img src={moodPic} alt="Your mood"></img>
+      <div>
+        <button data-mood="happy" onClick={changeMood}>
+          happy :)
+        </button>
+        <button data-mood="ok" onClick={changeMood}>
+          just ok :|
+        </button>
+        <button data-mood="sad" onClick={changeMood}>
+          sad :(
+        </button>
+      </div>
     </div>
   );
 }
